7facdc0e166fc266ff910b6a4df6792885a6d619
ashaydharmik <ashaydharmik807@gmail.com>
2023-10-09 22:12:56 +0530

    completed profile section

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./news.scss";
 import profile from "../assets/profile.png";
 const News = () => {
+const [cardTitle, setCardTitle] = useState([])
+
+useEffect(()=>{
+  const storedCardTitle = localStorage.getItem("MovieDetails")
+  if(storedCardTitle){
+    const showTitle = JSON.parse(storedCardTitle)
+    setCardTitle(showTitle)
+  }
+},[])
+
   return (
     <>
       <section className="main-container">
@@ -18,19 +28,24 @@ const News = () => {
                   <p className="username">ashay</p>
                 </div>
                 <div className="movie-info">
-                  <button>Action</button>
-                  <button>Romance</button>
-                  <button>Triller</button>
-                  <button>Horror</button>
-                  <button>Horror</button>
-                  <button>Horror</button>
-                  <button>Horror</button>
-                  <button>Horror</button>
+                  {
+                    cardTitle.map((item,id)=>{
+                      return(
+                        <>
+                        <button key={id}>{item}</button>
+                        </>
+                      )
+                    })
+                  }
                 </div>
               </div>
             </div>
             <div className="temp-box">
-
+                <div className="upper">
+                  <p>2-02-2023</p>
+                  <p>03:33 PM</p>
+                </div>
+                <div className="lower"></div>
             </div>
           </div>
           <div className="news-container"></div>
